72209926510c3a5aa14b6eac0e3db01782ac2297
Angela <angeladejuan55@gmail.com>
2025-05-29 00:20:06 +0800

    Simulate typewriter animation with delay

--- typewriter.py
+++ typewriter.py
@@ -1,3 +1,8 @@
 import time 
 
-class TypeWriter: 
+class TypeWriter:
+    def type_writer(self, message, delay=0.04): 
+        for character in message:  
+            print(character, end='', flush=True)  
+            time.sleep(delay)  
+        print()
