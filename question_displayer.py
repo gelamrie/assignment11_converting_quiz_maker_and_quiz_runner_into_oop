104d4871f5432e4292e6fc798ddae0235fd0447e
Angela <angeladejuan55@gmail.com>
2025-05-29 00:58:49 +0800

    Create table for choices

--- question_displayer.py
+++ question_displayer.py
@@ -8,3 +8,7 @@ class QuestionDisplayer:
 
     def display_question(self, question_data, question_number):  
         self.console.print(f"\n[bold blue]Question {question_number + 1}:[/bold blue] {question_data['question']}")
+
+        choices_table = Table(box=box.SIMPLE)  
+        choices_table.add_column("Letter", justify="center")
+        choices_table.add_column("Option", justify="left")
