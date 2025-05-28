ed418806a03c9d76534433bf1082471942fdbf22
Angela <angeladejuan55@gmail.com>
2025-05-29 00:57:49 +0800

    Display questions

--- question_displayer.py
+++ question_displayer.py
@@ -5,3 +5,6 @@ from rich import box
 class QuestionDisplayer:  
     def __init__(self):  
         self.console = Console()
+
+    def display_question(self, question_data, question_number):  
+        self.console.print(f"\n[bold blue]Question {question_number + 1}:[/bold blue] {question_data['question']}")
