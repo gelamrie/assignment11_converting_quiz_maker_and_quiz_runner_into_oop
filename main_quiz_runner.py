e1267f82f2102f4f99e5807c94607a4f9499c299
Angela <angeladejuan55@gmail.com>
2025-05-29 01:18:45 +0800

    Main quiz runner class to run the program

--- main_quiz_runner.py
+++ main_quiz_runner.py
@@ -2,4 +2,12 @@ from rich.console import Console
 from typewriter import TypeWriter
 from file_chooser import FileChooser
 from question_loader import QuestionLoader
-from quiz import Quiz
+from quiz import Quiz 
+
+class MainQuizRunner:  
+    def __init__(self):
+        self.console = Console()
+        self.typewriter = TypeWriter()
+        self.file_chooser = FileChooser()
+        self.question_loader = QuestionLoader()
+        self.quiz = Quiz()
