5bf1376436b9fedf43e5076d771051bbfefa9ea6
Angela <angeladejuan55@gmail.com>
2025-05-29 00:59:41 +0800

    Map letters to choices

--- question_displayer.py
+++ question_displayer.py
@@ -12,3 +12,7 @@ class QuestionDisplayer:
         choices_table = Table(box=box.SIMPLE)  
         choices_table.add_column("Letter", justify="center")
         choices_table.add_column("Option", justify="left")
+
+        choice_letters = ["A", "B", "C", "D"]  
+        for index, choice_text in enumerate(question_data["choices"]):
+            choices_table.add_row(choice_letters[index], choice_text)
