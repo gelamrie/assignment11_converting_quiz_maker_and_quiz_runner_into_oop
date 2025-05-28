bc822c69f77f6e4b4d1d0a1f840c0d7ab3f0d3d5
Angela <angeladejuan55@gmail.com>
2025-05-29 01:07:28 +0800

    Quiz execution class

--- quiz.py
+++ quiz.py
@@ -2,3 +2,9 @@ import random
 from rich.console import Console
 from question_displayer import QuestionDisplayer
 from answer_prompt import AnswerPrompt
+
+class Quiz: 
+    def __init__(self):
+        self.console = Console()
+        self.question_displayer = QuestionDisplayer()
+        self.answer_prompt = AnswerPrompt()
