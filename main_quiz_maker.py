f7c0d3d5eeebad77af3ca971e41e2d5482c8e9af
Angela <angeladejuan55@gmail.com>
2025-05-28 23:19:05 +0800

    Represent WelcomeDisplay

--- main_quiz_maker.py
+++ main_quiz_maker.py
@@ -3,4 +3,6 @@ from welcome_display import WelcomeDisplay
 from question_manager import QuestionManager
 
 class MainQuizMaker:
-    def run(self):   
+    def run(self):
+        welcome_display = WelcomeDisplay()       
+        welcome_display.welcome_screen()   
