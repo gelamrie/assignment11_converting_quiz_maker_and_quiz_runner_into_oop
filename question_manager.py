2910b53683e3eaf30203bc4e2387217f9a9f8c5e
Angela <angeladejuan55@gmail.com>
2025-05-28 23:04:29 +0800

    Ensure file name has .txt entension

--- question_manager.py
+++ question_manager.py
@@ -2,3 +2,6 @@ from colorama import Fore
 from question import Question 
 
 class QuestionManager: 
+    def __init__(self, file_name):
+        if not file_name.endswith(".txt"):    
+            file_name += ".txt"
