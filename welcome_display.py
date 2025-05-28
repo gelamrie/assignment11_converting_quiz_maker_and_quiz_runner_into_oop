89a8d2d4576c69564e83312e9c0cbaa9a696d640
Angela <angeladejuan55@gmail.com>
2025-05-28 22:08:44 +0800

    Simulate typing animation with color and centered text

--- welcome_display.py
+++ welcome_display.py
@@ -3,4 +3,11 @@ import time, sys
 
 init(autoreset=True)
 
-class WelcomeDisplay:  
+class WelcomeDisplay:
+     def typing_effect_centered(self, text, width=60, delay=0.04, color=Fore.RED + Style.BRIGHT):
+        centered_text = text.center(width)
+        for char in centered_text:
+            sys.stdout.write(color + char)
+            sys.stdout.flush()
+            time.sleep(delay)
+        print() 
