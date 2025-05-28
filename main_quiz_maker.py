e34ed6f7609310ad0ece7bc8a190bd13e9fc1bda
Angela <angeladejuan55@gmail.com>
2025-05-28 23:19:56 +0800

    Ask for file name

--- main_quiz_maker.py
+++ main_quiz_maker.py
@@ -5,4 +5,7 @@ from question_manager import QuestionManager
 class MainQuizMaker:
     def run(self):
         welcome_display = WelcomeDisplay()       
-        welcome_display.welcome_screen()   
+        welcome_display.welcome_screen()  
+
+        file_name = input(Fore.LIGHTCYAN_EX + "Enter the file name to save your quiz (e.g., myquiz.txt): ").strip()
+        question_manager = QuestionManager(file_name)
