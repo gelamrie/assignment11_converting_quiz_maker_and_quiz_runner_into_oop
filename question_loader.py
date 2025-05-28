cee24fa1cd8e6ef96411a8192e906b920399fabd
Angela <angeladejuan55@gmail.com>
2025-05-29 00:43:04 +0800

    Storing questions in a list ad adding index tracker for reading lines

--- question_loader.py
+++ question_loader.py
@@ -7,3 +7,6 @@ class QuestionLoader:
     def load_questions_from_text(self, file_path):  
         with open(file_path, 'r', encoding='utf-8') as file:  
             raw_lines = [line.strip() for line in file.readlines()]  
+
+        question_list = []  
+        line_index = 0  
