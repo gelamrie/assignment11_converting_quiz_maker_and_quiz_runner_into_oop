de5cf6b9dfcc492b0e98859706dd743049b499a3
Angela <angeladejuan55@gmail.com>
2025-05-29 00:35:21 +0800

    Allow user to choose quiz text file

--- file_chooser.py
+++ file_chooser.py
@@ -1,4 +1,12 @@
 import tkinter as tk  
 from tkinter import filedialog  
 
-class FileChooser: 
+class FileChooser:
+    def choose_file(self):  
+        root = tk.Tk()  
+        root.withdraw()  
+        file_path = filedialog.askopenfilename(  
+            title="Select Quiz Bank File",  
+            filetypes=[("Text files", "*.txt")]  
+        )
+        return file_path
